10ff3aeeb541268654206b759cc9409ee0c6f514
Thomas Glauser <thomas@glauser.dev>
2020-12-17 20:36:28 +0100

    img alt

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,10 +27,11 @@ const IndexPage = () => {
     <div>
       <div class="bg-gray-900 w-screen h-screen flex justify-center items-center">
         <div class="px-8 md:px-16 bg-white rounded shadow-xl">
-          <img src={headerImage} class="w-64"></img>
+          <img src={headerImage} class="w-64" alt="Header"></img>
           <img
             src={portfolioImage}
             class="h-24 w-24 rounded-full transform -translate-y-1/2 m-auto"
+            alt="Profile"
           ></img>
           <h1 class="text-center font-semibold font-semiBold">{headerText}</h1>
           <h2 class="text-center font-light text-gray-500">{subText}</h2>
