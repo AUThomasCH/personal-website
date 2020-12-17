c98212657c24f5f56dd3e91e0db5c14e6926caab
Thomas Glauser <thomas@glauser.dev>
2020-12-17 14:34:00 +0100

    Install tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    "gatsby-plugin-manifest",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
   ],
 };
