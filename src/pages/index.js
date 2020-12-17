3118ffc949de8b54319b4f2e1537d6ac378c6cea
Thomas Glauser <thomas@glauser.dev>
2020-12-17 15:15:10 +0100

    basic layout init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-// markup
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Hello World</h1>
-    </main>
+    <div>
+      <div class="bg-gray-900 w-screen h-screen flex justify-center items-center">
+        <div class="py-8 px-8 md:px-16 bg-white rounded"></div>
+      </div>
+    </div>
   );
 };
 
