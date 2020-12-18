86a72ea041a4b6dc3c1eeef7e72e4a5b4d248ed9
Thomas Glauser <thomas@glauser.dev>
2020-12-18 13:06:42 +0100

    add SEO tools (sitemap and robots)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-postcss"],
+  siteMetadata: {
+    siteUrl: "https://www.thomas-glauser.ch",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-robots-txt",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://www.thomas-glauser.ch",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+  ],
 };
